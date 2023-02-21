ee98d20ed7d8cde93fb100aa6baba716e3223bde
Rose Reyes <36457350+RoseReyes@users.noreply.github.com>
2023-02-21 09:45:48 -0800

    using map function to iterate over elements in react (#8)

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-// passing a function instead of an object when updating state
-// setState is asynchronous
+
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      name: {firstName: 'Rose', lastName: 'Reyes'},
-      company: 'ZTM'
-    }
+      monsters: [
+         {
+          name: 'Linda'
+         },
+         {
+          name: 'Frank'
+         },
+         {
+          name: 'Jacky'
+         }
+      ],
+    };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name.firstName} {this.state.name.lastName} , I work at {this.state.company}</p>
-          <button onClick={() => {
-            this.setState(() => {
-              return {
-                name: {firstName: 'Andrei', lastName: 'Negaoia'}
-              }
-            });
-          }}
-          >Change Name</button>
-        </header>
+        {this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>;
+        })}
       </div>
     ); 
   }
