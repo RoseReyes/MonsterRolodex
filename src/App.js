95bd266678595a78d8bf4b22a754cd1847477571
Rose Reyes <36457350+RoseReyes@users.noreply.github.com>
2023-02-21 16:56:19 -0800

    order of code execution and which trigger the rendering and re-rendering (#13)

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 
 
 class App extends Component {
+  // 1. initialize the state
   constructor() {
     super();
-
     this.state = {
       monsters: [],
     };
   }
 
+  // 3. runs after the component mounts
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState(() => {
         return { monsters: users}
-      }));
+      }
+    ));
   }
 
+  // 2. this runs after the constructor and render the initial UI
+  // 4: get re-renders when the state object change
   render() {
     return (
       <div className="App">
