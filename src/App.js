563b181fe6359966510cbf17ec660b5d23090a8b
Rose Reyes <roseannlorinreyes@gmail.com>
2023-02-21 20:48:06 -0800

    removing unanimous function and destructuring

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 
 
 class App extends Component {
-  // 1. initialize the state
   constructor() {
     super();
     this.state = {
@@ -12,40 +11,49 @@ class App extends Component {
     };
   }
 
-  // 3. runs after the component mounts
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState(() => {
-        return { monsters: users}
+        return { monsters: users }
       }
-    ));
+      ));
   }
 
-  // 2. this runs after the constructor and render the initial UI
-  // 4: get re-renders when the state object change
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField }
+    }
+    );
+  };
+
   render() {
-    
-    const filteredMonsters = this.state.monsters.filter((monster) => { 
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    // destructuring; it will remove calling this everywhere; cast it to variable
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
       <div className="App">
-      <input className='search-box' type='search' placeholder='search monster' onChange={(event) => {
-         const searchField = event.target.value.toLocaleLowerCase();
-         // update the searchField  
-         this.setState(() => {
-              return { searchField }
-            }
-          );
-        }} 
-      />
-      {filteredMonsters.map((monster) => { // display filtered list
-          return <div key={monster.id}><h1>{monster.name}</h1></div>;
-      })}
-      </div> 
-    ); 
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monster'
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+            );
+        })}
+      </div>
+    );
   }
 }
 
